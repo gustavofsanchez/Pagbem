0089c3765e2051b2320188b1123aa8bee397717d
gustavofsanchez <gustavofreitassanchez@gmail.com>
2023-10-04 19:16:56 -0300

    CRUD completo
    Primeiro crud completo

--- pagBem/pagControle/urls.py
+++ pagBem/pagControle/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-
+    path('cadastro_cliente/', views.cadastro_cliente, name='cadastro_cliente'),
+    path('deletar_cliente/<str:pk>/', views.deletar_cliente, name='deletar_cliente'),
+    path('editar_cliente/<str:pk>/', views.editar_cliente, name='editar_cliente'),
+    path('editar_cliente_modal/<str:pk>/', views.editar_cliente_modal, name='editar_cliente_modal'),
 ]
--- pagBem/pagControle/views.py
+++ pagBem/pagControle/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from datetime import date
+import json
+from django.shortcuts import redirect, render
 
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 from pagControle.models import Cliente
 
@@ -9,4 +11,46 @@ def index(request):
     context = {'clientes':clientes}
     return render(request, 'index.html', context)
 
+def cadastro_cliente(request):
+    nome = request.POST['nome']
+    login = request.POST['login']
+    senha = request.POST['senha']
+    novoCliente = Cliente(
+        nome = nome,
+        login = login,
+        senha = senha,
+        ativo = False,
+        vencimento =  date.today(),
+        plano = None
+    )
+    novoCliente.save()
+    return redirect('http://127.0.0.1:8000/')
 
+def deletar_cliente(request, pk):
+    cliente = Cliente.objects.get(id=pk)
+    cliente.delete()
+    return redirect('http://127.0.0.1:8000/')
+
+def editar_cliente(request, pk):
+    cliente = Cliente.objects.get(id=pk)
+    if request.method == 'POST':
+        nome = request.POST['nome']
+        login = request.POST['login']
+        senha = request.POST['senha']
+        cliente.nome = nome
+        cliente.login = login
+        cliente.senha = senha
+        cliente.save() 
+        return redirect('http://127.0.0.1:8000/')
+    context = {'cliente':cliente}
+    return render(request, 'editar_cliente.html', context)
+
+
+def editar_cliente_modal(request, pk):
+    cliente = Cliente.objects.get(id=pk)
+    newdata = {
+			"nome": cliente.nome,
+            "login": cliente.login,
+            "senha": cliente.senha
+		}
+    return JsonResponse(newdata)
